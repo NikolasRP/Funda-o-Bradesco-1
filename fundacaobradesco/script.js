11bcb3ac8f5831c565b198a49edef0a3b35bc94f
Nikolas R. Pires <nikipires1985@gmail.com>
2024-11-19 12:00:26 -0300

    Add files via upload

---
+++ fundacaobradesco/script.js
@@ -0,0 +1,15 @@
+const swither = document.querySelector('.btn');
+
+swither.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+
+var className = document.body.className;
+if (className == "light-theme") {
+    this.textContent = "dark";
+}else {
+    this.textContent = "light";
+}
+
+console.log('current class name' + className);
+});
